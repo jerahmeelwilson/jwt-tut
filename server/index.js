9d75deb8363dd868058732931e74a30c990b65f7
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2022-01-22 15:27:59 -0600

    Added routes for register and login that respond with JWTs

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-const sequelize = require('./sequelize');
-const {connect} = require('./testConnection');
-require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+const sequelize = require("./sequelize");
+
 //middleware
 app.use(express.json());
 app.use(cors());
 
-
 //Routes
 
+//Register and login routes
+
+app.use("/auth", require("./routes/jwtAuth"));
+
 const seed = (req, res) => {
-    sequelize.query(`
+  sequelize
+    .query(
+      `
         drop table if exists users;
         
         create table users(
@@ -22,21 +26,15 @@ const seed = (req, res) => {
             user_email varchar(255) not null,
             user_password varchar(255) not null
         );
-        
-        insert into users (user_name, user_email, user_password)
-        values('jerahmeel','jerahmeelwilson@gmail.com','jimmy123');
-            
-
-    `).then(() =>{
-        console.log("DB Seeded!")
-        res.sendStatus(200);
-    }).catch((err) => console.log("error seeding db", err))
-}
-
+    `
+    )
+    .then(() => {
+      console.log("DB Seeded!");
+      res.sendStatus(200);
+    })
+    .catch((err) => console.log("error seeding db", err));
+};
 
 app.post("/seed", seed);
 
-
-
-app.listen(4001, () => console.log("Server is running on post 4001")
-);
+app.listen(4001, () => console.log("Server is running on post 4001"));
---
+++ server/routes/jwtAuth.js
@@ -0,0 +1,78 @@
+const router = require("express").Router();
+const sequelize = require("../sequelize");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
+
+
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    //1. destructure req.body (name, email, password)
+    const { name, email, password } = req.body;
+    //2. check if user exist (if user exist then throw error)
+    const user = await sequelize.query(
+      `select * from users where user_email = '${email}'`
+    );
+    if (user[1].rowCount !== 0) {
+      return res.status(401).send("User already exist");
+    }
+    //3. Bcrypt the user password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //4. Enter the new user inside our database
+    await sequelize.query(
+      `INSERT INTO users (user_name, user_email, user_password) 
+      VALUES (
+          '${name}',
+          '${email}',
+          '${passwordHash}')`
+    );
+    const newUser = await sequelize.query(`SELECT user_id, user_name, user_email FROM users WHERE user_name = '${name}';`
+    );
+    //5. generating our jwt token
+  
+    const token = jwtGenerator(newUser[0][0].user_id);
+    res.json({token});
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+
+//Login route
+router.post('/login', async (req,res) => {
+    try {
+        //1. destructure the req.body
+
+        const {email, password} = req.body;
+
+
+        //2. check if user doesn't exist (if not then we throw error)
+        const user = await sequelize.query(`SELECT * FROM users WHERE user_email = '${email}'`);
+
+        //3. check if incoming password is the same as database password
+        if (user[1].rowCount === 0){
+            return res.status(401).send("Password or Email is incorrect");
+        }
+
+        const validPassword = await bcrypt.compare(password,user[0][0].user_password);
+
+        if(!validPassword){
+            return res.status(401).send("Password is incorrect");
+        }
+        //4. give them the jwt token
+        const token = jwtGenerator(user[0][0].user_id)
+
+        res.json({token});
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+module.exports = router;
